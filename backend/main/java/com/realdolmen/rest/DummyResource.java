c056b6cdb3730c297a47c0c6d9c4f16e331efa18
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-10-20 15:43:36 +0200

    Run JaxRS via embedded server

---
+++ backend/main/java/com/realdolmen/rest/DummyResource.java
@@ -0,0 +1,16 @@
+package com.realdolmen.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/dummy")
+public class DummyResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTopic() {
+        return "Hello Jersey Plain";
+    }
+}
